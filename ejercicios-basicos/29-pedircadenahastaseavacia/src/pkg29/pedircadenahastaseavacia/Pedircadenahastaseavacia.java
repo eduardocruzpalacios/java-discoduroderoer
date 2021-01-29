e50ac17f9111e63b31c76d5a67dcd6d50daaa997
Eduardo de la Cruz Palacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-01-29 16:51:17 +0100

    29-corregido

--- ejercicios-basicos/29-pedircadenahastaseavacia/src/pkg29/pedircadenahastaseavacia/Pedircadenahastaseavacia.java
+++ ejercicios-basicos/29-pedircadenahastaseavacia/src/pkg29/pedircadenahastaseavacia/Pedircadenahastaseavacia.java
@@ -4,12 +4,16 @@ public class Pedircadenahastaseavacia {
     public static void main(String[] args) {
         Scanner t = new Scanner(System.in);
         String cadena;
-        
+        String cadena_total = "";
+        int contador = 1;
         do
         {
             System.out.println("Dame una cadena");
             cadena = t.nextLine();
+            cadena_total += contador + ".- " + cadena + "\n";
+            contador++;
         }
         while(!cadena.equals(""));
+        System.out.println(cadena_total);
     }
 }
