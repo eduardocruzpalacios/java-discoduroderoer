fbea5ce5db1261af74e9c72cfafc0fafbfaac042
Eduardo de la Cruz Palacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-01-30 20:30:26 +0100

    034-updated

--- ejercicios-basicos/34-string-separarpalabras/src/pkg34/string/separarpalabras/StringSepararpalabras.java
+++ ejercicios-basicos/34-string-separarpalabras/src/pkg34/string/separarpalabras/StringSepararpalabras.java
@@ -2,10 +2,10 @@ package pkg34.string.separarpalabras;
 public class StringSepararpalabras {
     public static void main(String[] args)
     {
-        String frase = "Escribe una frase";
+        String frase = "Escribe una frase. Hola, Bb + Aa [jj, 447]";
          
         // Arrray de String guarda palabras de frase
-        String palabras[] = frase.split(" ");
+        String palabras[] = frase.split("[ ,.]");
          
         for(int i=0;i<palabras.length;i++)
         {
