6ca952db541918a43a8837440a0b265a38f64f46
Eduardo de la Cruz Palacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-02-04 19:11:31 +0100

    matrices05-iniciado

---
+++ matrices/05-matriz-pokemon-tipos/src/pkg05/matriz/pokemon/tipos/MatrizPokemonTipos.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg05.matriz.pokemon.tipos;
+public class MatrizPokemonTipos {
+    public static void main(String[] args) {
+        
+    }
+}
